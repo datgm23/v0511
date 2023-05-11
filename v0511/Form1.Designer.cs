9ff41e937525706848dfc2f5ba0881772702d2bf
YuTanaka <tanakaedu@users.noreply.github.com>
2023-05-11 16:08:53 +0900

    プロパティ

--- v0511/Form1.Designer.cs
+++ v0511/Form1.Designer.cs
@@ -29,23 +29,40 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(12, 12);
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Maroon;
+            button1.Location = new Point(212, 133);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(200, 97);
             button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "あいさつ";
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(447, 242);
+            button2.Name = "button2";
+            button2.Size = new Size(165, 107);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Visible = false;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(624, 361);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +72,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
